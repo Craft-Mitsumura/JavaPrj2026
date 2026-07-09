334732e0e7980b83f9d89b54d5639bab50bf7285
小松原愛 <komatsubara.ai127@gmail.com>
2026-07-09 17:39:36 +0900

    アイテム一覧

--- shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
+++ shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.entity.Category;
 import jp.co.sss.shop.entity.Favorite;
 import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.entity.Promotions;
@@ -180,7 +181,12 @@ public class ClientItemShowController {
 		model.addAttribute("page", itemPage);
 
 		model.addAttribute("categories", categoryRepository.findAll());
+		
+		Category category =
+		        categoryRepository.findByIdAndDeleteFlag(id, 0);
 
+		model.addAttribute("category", category);
+	
 		// ページ移動時にカテゴリを保持する
 		model.addAttribute("categoryId", id);
 
@@ -231,6 +237,7 @@ public class ClientItemShowController {
 		
 //		ページの小見出しを作成する
 		model.addAttribute("allsubtittle", "すべての商品");
+		model.addAttribute("allsubtittleex", "avoirの商品をご紹介します。");
 
 		Page<Item> itemPage = itemRepository.findAll(
 				PageRequest.of(page, 20));
